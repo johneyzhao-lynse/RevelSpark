ea710e3d39198b629b8a5ebc6f089a2cbb6ff8a4
qijian <qijian.wan@geekdance.ai>
2025-08-15 10:01:46 +0800

    feat: 添加 iOS 的 well-known 文件夹，完善通用链接的能力

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: 'public', // 将.well-known文件夹放在public目录下
+  build: {
+    outDir: 'dist'
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
